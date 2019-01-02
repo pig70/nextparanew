b00e3a95337eb7f7018d40134e2e559e8b10fe73
richy <richyarwood@gmail.com>
2019-01-02 20:28:28 +0000

    Styled stories without paragraphs and changed input styling

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,7 +1,8 @@
-from django.db import models
-from django.utils import timezone
-from django.template.defaultfilters import truncatewords
 from django.contrib.auth.models import User
+from django.db import models
+from django.template.defaultfilters import truncatewords
+from django.utils import timezone
+
 from userprofile.models import AuthorProfile
 
 #Model for the original story
--- userprofile/models.py
+++ userprofile/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class AuthorProfile(models.Model):
-    author_image = models.ImageField(upload_to='mainsite/static/images/')
+    author_image = models.ImageField(upload_to='mains˙ite/static/images/')
     author = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True, related_name='author_profile_image')
     def __str__(self):
         return self.author.get_full_name()
