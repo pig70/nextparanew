5d97bcab8af87c8979926528ce6077275282b4bc
richy <richyarwood@gmail.com>
2019-01-15 15:51:20 +0000

    Added a character count to paragraph inputs

--- mainsite/static/js/base.js
+++ mainsite/static/js/base.js
@@ -1,12 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-$('body #article-like-heart').click(function() {
-            $(this)
-                .toggleClass('far fa-heart fa-lg')
-                .toggleClass('fas fa-heart fa-lg');
-        });
-$('#article-watchers-eye-not-watched').click(function() {
-            $(this)
-                .toggleClass('far fa-eye fa-lg')
-                .toggleClass('fas fa-eye fa-lg');
-        });
+    $('body #article-like-heart').click(function () {
+        $(this)
+            .toggleClass('far fa-heart fa-lg')
+            .toggleClass('fas fa-heart fa-lg');
     });
+    $('#article-watchers-eye-not-watched').click(function () {
+        $(this)
+            .toggleClass('far fa-eye fa-lg')
+            .toggleClass('fas fa-eye fa-lg');
+    });
+});
+
+// Character count
+
+var textarea = document.querySelector("textarea");
+var maxlength = textarea.getAttribute("maxlength");
+
+textarea.addEventListener("input", function () {
+    var currentLength = this.value.length;
+    if ( currentLength == maxlength ) {
+        document.getElementById("count").innerHTML = "No more characters left";
+    } else {
+        document.getElementById("count").innerHTML = (maxlength - currentLength) + " characters left";
+    }
+});
