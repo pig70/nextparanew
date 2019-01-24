7c890ad484c2d14a0d548f772b0365df29d45782
richy <richyarwood@gmail.com>
2019-01-24 23:12:02 +0000

    Added watcher, profile edit and dropdown nav

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
 
     path('stories/<slug:slug>/<int:pk>/', views.story, name='story'),
     path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('registration-complete/', views.register_complete, name='registration_complete'),
+    path('registration/register/', views.register, name='register'),
+    path('registration/registration-complete/', views.register_complete, name='registration_complete'),
     path('all-paragraphs/', views.all_paragraphs, name='all_paragraphs'),
-    path('start-a-story/', views.start_a_story, name='start_story')
+    path('start-a-story/', views.start_a_story, name='start_story'),
+    path('registration/edit/', views.edit_profile, name='edit_profile')
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
